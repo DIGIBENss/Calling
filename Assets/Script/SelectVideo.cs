85e4b18bf3309cf47077621057826fc7ada16e86
alastor <114815838+Alastor606@users.noreply.github.com>
2023-11-13 19:56:19 +0300

    Scam

--- Assets/Script/SelectVideo.cs
+++ Assets/Script/SelectVideo.cs
@@ -1,10 +1,8 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using System.Threading.Tasks;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
+
 
 public class SelectVideo : MonoBehaviour
 {
@@ -13,31 +11,56 @@ public class SelectVideo : MonoBehaviour
     [SerializeField] private GameObject _textmain;
     [SerializeField] private GameObject _videoplayerobject;
     [SerializeField] private string[] _videoFileName =  new string[] { "1.mp4", "2.mp4", "3.mp4" };
+    private double Length = 0;
+    
     public void Select(int index) => StartCoroutine(SelectC(index));
+    private void OnVideoPrepared(VideoPlayer player)
+    {
+        Length = player.length;
+        print(player.length);
+    }
 
     private IEnumerator SelectC(int index)
     {
-      if (index > _videoFileName.Length) yield break;
+        if (index > _videoFileName.Length) yield break;
+        if(index == 2)
+        {
+            //тут реклама типа емае
+        }
         string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, _videoFileName[index]);
         Notnecessary();
-      yield return new WaitForSeconds(0.15f);
-      _player.url = videoPath;
-      _player.Play();
-      var i = 0;
-        while (i < _player.clip.length)
+        _player.url = videoPath;
+        _player.Prepare();
+        bool isLoading = true;
+        _player.prepareCompleted += (value) =>
+        {
+            OnVideoPrepared(value);
+            isLoading = false;
+        };
+        
+        while (isLoading)
+        {
+            yield return null;
+        }
+
+        _player.Play();
+        var i = 0;
+        while (i < Length)
         {
             yield return new WaitForSeconds(1);
             i++;
         }
-      Deceline();
+        Deceline();
     }
+
     public void Deceline()
     {
         _textmain.SetActive(true);
         _videoplayerobject.SetActive(false);
         _menucontent.SetActive(true);
-      _player.Stop();
+        _player.Stop();
     }
+
     private void Notnecessary()
     {
         _videoplayerobject.SetActive(true);
