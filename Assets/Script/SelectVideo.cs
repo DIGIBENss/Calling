ca5d5b5eec032cb2f999d1abca6570a303cc01e8
Master_Shadows <pozitivmr15@gmail.com>
2023-11-14 21:21:05 +0300

    0.14

--- Assets/Script/SelectVideo.cs
+++ Assets/Script/SelectVideo.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Video;
-
+using YG;
 
 public class SelectVideo : MonoBehaviour
 {
@@ -10,25 +10,36 @@ public class SelectVideo : MonoBehaviour
     [SerializeField] private GameObject _menucontent;
     [SerializeField] private GameObject _textmain;
     [SerializeField] private GameObject _videoplayerobject;
-    [SerializeField] private string[] _videoFileName =  new string[] { "1.mp4", "2.mp4", "3.mp4" };
+    [SerializeField] private AudioManager _callbell;
+    [SerializeField] private GameObject _callbellobject;
+    [SerializeField] private IconCall _iconcall;
+    [SerializeField] private string[] _videoFileName =  new string[] { "1.mp4", "2.mp4", "3.mp4", "4.mp4" };
     private double Length = 0;
+
+    [SerializeField] YandexGame _sdk;
     
     public void Select(int index) => StartCoroutine(SelectC(index));
     private void OnVideoPrepared(VideoPlayer player)
     {
         Length = player.length;
-        print(player.length);
     }
 
     private IEnumerator SelectC(int index)
     {
         if (index > _videoFileName.Length) yield break;
-        if(index == 2)
+        if(index == 1 || index == 3)
         {
-            //тут реклама типа емае
+            _sdk._RewardedShow(0);
+            yield return new WaitForSeconds(1);
+            
         }
+        Debug.Log(index);
+        _iconcall.IndexImage(index);
         string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, _videoFileName[index]);
         Notnecessary();
+        TheBell();
+        yield return new WaitForSeconds(3f);
+        Notbell();
         _player.url = videoPath;
         _player.Prepare();
         bool isLoading = true;
@@ -63,8 +74,18 @@ public class SelectVideo : MonoBehaviour
 
     private void Notnecessary()
     {
-        _videoplayerobject.SetActive(true);
         _textmain.SetActive(false);
         _menucontent.SetActive(false);
+    }
+    private void TheBell()
+    {
+        _callbell.PlaySound(1);
+        _callbellobject.SetActive(true);
+    }
+    private void Notbell()
+    {
+        _callbellobject.SetActive(false);
+
+        _videoplayerobject.SetActive(true);
     }
 }
