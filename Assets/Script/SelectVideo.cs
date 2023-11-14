f7d406d9b5ccffa7ffe1636d947f64d5f1576d33
alastor <114815838+Alastor606@users.noreply.github.com>
2023-11-14 21:32:46 +0300

    Babki Fix

--- Assets/Script/SelectVideo.cs
+++ Assets/Script/SelectVideo.cs
@@ -19,10 +19,8 @@ public class SelectVideo : MonoBehaviour
     [SerializeField] YandexGame _sdk;
     
     public void Select(int index) => StartCoroutine(SelectC(index));
-    private void OnVideoPrepared(VideoPlayer player)
-    {
+    private void OnVideoPrepared(VideoPlayer player) =>
         Length = player.length;
-    }
 
     private IEnumerator SelectC(int index)
     {
@@ -33,26 +31,17 @@ public class SelectVideo : MonoBehaviour
             yield return new WaitForSeconds(1);
             
         }
-        Debug.Log(index);
         _iconcall.IndexImage(index);
+
         string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, _videoFileName[index]);
+        _player.url = videoPath;
+        _player.Prepare();
+        _player.prepareCompleted += (value) => OnVideoPrepared(value);
+
         Notnecessary();
         TheBell();
         yield return new WaitForSeconds(3f);
         Notbell();
-        _player.url = videoPath;
-        _player.Prepare();
-        bool isLoading = true;
-        _player.prepareCompleted += (value) =>
-        {
-            OnVideoPrepared(value);
-            isLoading = false;
-        };
-        
-        while (isLoading)
-        {
-            yield return null;
-        }
 
         _player.Play();
         var i = 0;
